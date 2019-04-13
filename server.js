f63e2c70fb1793f0230dea85cd8be643e00814c3
Alexander <alexander.behrmann@hotmail.com>
2019-04-13 13:33:54 -0700

    Setup of the express server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 
+const express = require('express');
+const port = 4000;
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('This is the server');
+});
+
+server.listen(port, () => {
+    console.log("Listening on port " + port);
+});
